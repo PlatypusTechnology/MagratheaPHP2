6370bf4fae0e5664b32adda7f4df5d8017e57229
Paulo Martins <paulovelho@paulovelho.com>
2023-10-18 12:08:27 +0200

    typo fix

--- src/Admin/Api/AdminConfigApi.php
+++ src/Admin/Api/AdminConfigApi.php
@@ -20,7 +20,7 @@ class AdminConfigApi extends \Magrathea2\MagratheaApiControl {
 
 	public function SaveConfig($params) {
 		unset($params["magrathea_api"]);
-		unset($params["magrathea_api_method"]);w
+		unset($params["magrathea_api_method"]);
 		$configs = [];
 		$control = new AdminConfigControl();
 		foreach($params as $key => $value) {
