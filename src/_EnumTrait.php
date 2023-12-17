8d67a9912e9cdfdd0c109b33bba29d6301920016
Paulo Martins <paulovelho@paulovelho.com>
2023-12-17 19:03:17 -0300

    enum trait

---
+++ src/_EnumTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Magrathea2;
+
+trait Magrathea_Enum {
+	public static function names(): array {
+		return array_column(self::cases(), 'name');
+	}
+	public static function values(): array {
+		return array_column(self::cases(), 'value');
+	}
+	public static function array(): array {
+		return array_combine(self::values(), self::names());
+	}
+}
